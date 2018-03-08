a2f374e3ce3f9c5663b77c155b6a77f2fba51947
Kensuke Numakura <knmkr3gma@gmail.com>
2018-03-09 00:25:33 +0900

    run tests on CircleCI

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
-import genomelink
 from flask import Flask, flash, render_template, request, redirect, session, url_for
-app = Flask(__name__)
+import genomelink
 
 
-@app.route('/')
-def index():
-    authorize_url = genomelink.OAuth.authorize_url(scope=['report:eye-color report:beard-thickness report:morning-person'])
+def create_app():
+    app = Flask(__name__)
 
-    # Fetching a protected resource using an OAuth2 token if exists.
-    reports = []
-    if session.get('oauth_token'):
-        for name in ['eye-color', 'beard-thickness', 'morning-person']:
-            reports.append(genomelink.Report.fetch(name=name, population='european', token=session['oauth_token']))
+    @app.route('/')
+    def index():
+        authorize_url = genomelink.OAuth.authorize_url(scope=['report:eye-color report:beard-thickness report:morning-person'])
 
-    return render_template('index.html', authorize_url=authorize_url, reports=reports)
+        # Fetching a protected resource using an OAuth2 token if exists.
+        reports = []
+        if session.get('oauth_token'):
+            for name in ['eye-color', 'beard-thickness', 'morning-person']:
+                reports.append(genomelink.Report.fetch(name=name, population='european', token=session['oauth_token']))
 
-@app.route('/callback')
-def callback():
-    # The user has been redirected back from the provider to your registered
-    # callback URL. With this redirection comes an authorization code included
-    # in the request URL. We will use that to obtain an access token.
-    try:
-        token = genomelink.OAuth.token(request_url=request.url)
-    except genomelink.errors.GenomeLinkError as e:
-        flash('Authorization failed.')
+        return render_template('index.html', authorize_url=authorize_url, reports=reports)
+
+    @app.route('/callback')
+    def callback():
+        # The user has been redirected back from the provider to your registered
+        # callback URL. With this redirection comes an authorization code included
+        # in the request URL. We will use that to obtain an access token.
+        try:
+            token = genomelink.OAuth.token(request_url=request.url)
+        except genomelink.errors.GenomeLinkError as e:
+            flash('Authorization failed.')
+            return redirect(url_for('index'))
+
+        # At this point you can fetch protected resources but lets save
+        # the token and show how this is done from a persisted token in index page.
+        session['oauth_token'] = token
         return redirect(url_for('index'))
 
-    # At this point you can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token in index page.
-    session['oauth_token'] = token
-    return redirect(url_for('index'))
+    return app
 
 if __name__ == '__main__':
     # This allows us to use a plain HTTP callback.
@@ -38,5 +42,6 @@ if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
     # Run local server on port 5000.
+    app = create_app()
     app.secret_key = os.urandom(24)
     app.run(debug=True)
